b9216405a00fb281892972f010894ea6c9e9ed76
Lisa Wittmann <lisa.wittmann@student.hs-rm.de>
2021-11-11 10:48:25 +0100

    feat(#18): add simple tile representation

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -5,29 +5,54 @@
 <script lang="ts">
 import { defineComponent, onBeforeUnmount, onMounted } from "vue";
 import { useSceneService } from "@/service/SceneService";
-import { Vector3 } from "three";
+import { useTileService } from "@/service/TileService";
+import { vector } from "@/service/GeometryHelper";
 
 export default defineComponent({
   name: "scene",
   setup() {
     const { createScene, renderScene, insertCanvas, updateScene } =
       useSceneService();
+    const { createTile } = useTileService();
 
     // ---- testing data
-    const player = new Vector3(0, 1, 10);
-    const scene = createScene(player, true);
-    // ---- testing data end
+    const scene = createScene(vector(0, 1, 0), true);
+    const tileSize = 20;
+    scene.add(
+      createTile(
+        { width: tileSize, height: tileSize },
+        vector(0, 0, 0),
+        0x000000
+      )
+    );
+    // ----
 
     onMounted(() => {
       insertCanvas("scene");
       renderScene();
 
       window.addEventListener("resize", updateScene);
+      window.addEventListener("mousedown", onMouseDown);
+      window.addEventListener("mouseup", onMouseUp);
     });
 
     onBeforeUnmount(() => {
       window.removeEventListener("resize", updateScene);
+      window.removeEventListener("mousedown", onMouseDown);
+      window.removeEventListener("mouseup", onMouseUp);
     });
+
+    function onMouseDown(event: MouseEvent) {
+      window.addEventListener("mousemove", onMouseMove);
+    }
+
+    function onMouseMove(even: MouseEvent) {
+      console.log("dragging");
+    }
+
+    function onMouseUp(event: MouseEvent) {
+      window.removeEventListener("mousemove", onMouseMove);
+    }
   },
 });
 </script>
