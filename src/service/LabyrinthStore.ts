469fbba8eaebfd6c1f878f2af15d421ed351f456
Timo Striffler <timo.striffler@student.hs-rm.de>
2021-11-21 18:07:28 +0100

    feat(#20) Backendconnection - adjusting fetch function - creating dummydata

--- src/service/LabyrinthStore.ts
+++ src/service/LabyrinthStore.ts
@@ -1,12 +1,18 @@
 import { reactive, readonly } from "vue";
-import { Tile } from "./DataTypesforJSON";
+import { Tile, Orientation, Item } from "./Tile";
 
 /**
  * tileState: Constant to keep the tiles or store an errormessage
  */
-const tileState = reactive({
-    tiles: Array<Tile>(),
+const labyrinthState = reactive({
+    tileMap: new Map<number, Tile>([]),
+    startPosition: Array(),
+    endpoint: Number,
     errormessage: "" 
+})
+
+const tempTileRelations = reactive({
+    relationArray: Array<Array<number>>()
 })
 
 /**
@@ -14,7 +20,7 @@ const tileState = reactive({
  */
 async function updateLabyrinth() {
 
-    await fetch('/api/Labyrinth', {
+    await fetch(''/*'/api/Labyrinth'*/, {
         method: 'GET',
         //headers: { 'Authorization': `Bearer ${loginstate.jwttoken}` }
     })
@@ -27,13 +33,51 @@ async function updateLabyrinth() {
     })
     .then( (jsondata) => {
 
-        tileState.tiles = jsondata.tile;
+        labyrinthState.tileMap = jsondata.tileMap;
+
+        const tileMap = labyrinthState.tileMap;
+
+        tileMap.set(1, new Tile(1, new Map, [new Item(1)]));
+        tileMap.set(2, new Tile(2, new Map, [new Item(1)]));
+        tileMap.set(3, new Tile(3, new Map, [new Item(1)]));
+        tileMap.set(4, new Tile(4, new Map, [new Item(1)]));
+        tileMap.set(5, new Tile(5, new Map, [new Item(1)]));
+        tileMap.set(6, new Tile(6, new Map, [new Item(1)]));
+
+        const relArray = tempTileRelations.relationArray;
+
+        relArray.push(
+            [-1,2,-1,-1],
+            [-1,3,5,1],
+            [-1,2,-1,4],
+            [-1,-1,3,6],
+            [2,-1,-1,-1],
+            [4,-1,-1,-1]
+        );
+
+        tileMap.forEach(elem => {
+            for (let index = 0; index < 4; index++) {
+                const check = relArray[elem.getId()-1][index];
+                if(check != -1) {
+                    const secondTile = tileMap.get(check);
+
+                    if(typeof(secondTile) != undefined) {
+                        connectTiles(elem, secondTile, index);
+                    }
+                }    
+            }
+            //connectTiles(elem, )
+        });
 
     })
     .catch( (fehler) => {
-        tileState.errormessage = fehler;
+        labyrinthState.errormessage = fehler;
     });
 
+}
+
+function connectTiles(firstTile: Tile, secondTile: Tile, orientationRelation: Orientation) {
+    firstTile.getTileRelationMap().set(orientationRelation, secondTile);
 }
 
 /**
@@ -43,7 +87,7 @@ async function updateLabyrinth() {
 export function useLabyrinthStore()  {
 
     return {
-        tileState: readonly(tileState),
+        labyrinthState: readonly(labyrinthState),
         updateLabyrinth
     };
 }
