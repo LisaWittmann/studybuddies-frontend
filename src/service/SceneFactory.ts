8279b0437d3fff47aaedbe8222ea9e660073394a
Lisa Wittmann <lisa.wittmann@student.hs-rm.de>
2021-11-21 10:10:22 +0100

    feat(#18): add automatic scene updates

--- src/service/SceneFactory.ts
+++ src/service/SceneFactory.ts
@@ -63,7 +63,6 @@ function updateScene() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderScene();
 }
 
 /**
