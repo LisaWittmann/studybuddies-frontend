638636251409917a2b9b3b59f8bb3f34e40a1b34
Jasmin Steiner <jasmin.steiner@student.hs-rm.de>
2021-11-21 11:09:51 +0100

    added OrbitControls for camera rotation

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="scene"></div>
+  <!-- displaying camera position + lookAt for development-->
+  <div id="camera-vectors" style="position: absolute; top: 0; right: 0; background: grey; padding: 8px; color: white;">
+    <span>Free Camera Vectors:</span><br>
+    <span id="position"></span><br>
+    <span id="lookingAt"></span>
+  </div>
+  <!-------------------------------------------------------->
 </template>
 
 <script lang="ts">
@@ -11,12 +18,24 @@ import { vector } from "@/service/GeometryHelper";
 export default defineComponent({
   name: "scene",
   setup() {
-    const { createScene, renderScene, insertCanvas, updateScene } =
-      useSceneFactory();
+    const {
+      createScene,
+      animate,
+      renderScene,
+      insertCanvas,
+      updateScene,
+      updateCameraPosition
+    } = useSceneFactory();
     const { createTile } = useTileFactory();
 
+    //mousemovement
+    let isDragging = false;
+    let startX = 0;
+    let startY = 0;
+  
+
     // testing data
-    const scene = createScene(vector(0, 1, 0), true);
+    const scene = createScene(vector(0, 2, 0), true);
     const tileSize = 20;
     scene.add(
       createTile({ width: tileSize, height: tileSize }, vector(0, 0, 0))
@@ -25,29 +44,42 @@ export default defineComponent({
     onMounted(() => {
       insertCanvas("scene");
       renderScene();
+      animate();
 
       window.addEventListener("resize", updateScene);
-      window.addEventListener("mousedown", onMouseDown);
-      window.addEventListener("mouseup", onMouseUp);
+      window.addEventListener("mousedown", onMouseDown, false);
+      window.addEventListener("mousemove", onMouseMove, false);
+      window.addEventListener("mouseup", onMouseUp, false);
     });
+
+    //EventListeners-----
+    function onMouseDown(event: MouseEvent) {
+      //window.addEventListener("mousedown", onMouseDown);
+      console.log("mouse down", event.x, event.pageY);
+      startX = event.x;
+      startY = event.y;
+      isDragging = true;
+    }
+
+    function onMouseMove(event: MouseEvent) {
+      //window.addEventListener("mousemove", onMouseMove);
+      if (isDragging === true) {
+        console.log("mousemove", event.clientX, event.clientY);
+      }
+    }
+
+    function onMouseUp(event: MouseEvent) {
+      //window.removeEventListener("mouseup", onMouseDown);
+      console.log("mouse up", event.x, event.pageY);
+      isDragging = false;
+    }
 
     onBeforeUnmount(() => {
       window.removeEventListener("resize", updateScene);
       window.removeEventListener("mousedown", onMouseDown);
       window.removeEventListener("mouseup", onMouseUp);
-    });
-
-    function onMouseDown(event: MouseEvent) {
-      window.addEventListener("mousemove", onMouseMove);
-    }
-
-    function onMouseMove(event: MouseEvent) {
-      console.log("dragging");
-    }
-
-    function onMouseUp(event: MouseEvent) {
       window.removeEventListener("mousemove", onMouseMove);
-    }
+    });
   },
 });
 </script>
--- src/service/SceneFactory.ts
+++ src/service/SceneFactory.ts
@@ -1,8 +1,18 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 let scene: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
+
+//Camera
 let camera: THREE.PerspectiveCamera;
+let orbitControls: OrbitControls;
+
+//only for development------------------
+let cameraDirection: THREE.Vector3;
+let camPositionSpan: any;
+let camLookAtSpan: any;
+//--------------------------------------
 
 /**
  * creates new threejs 3D scene
@@ -14,10 +24,27 @@ function createScene(
   cameraPosition: THREE.Vector3,
   debug = false
 ): THREE.Scene {
+  //RENDERER-----------------
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //CAMERA-------------------
   const ratio = window.innerWidth / window.innerHeight;
   camera = new THREE.PerspectiveCamera(90, ratio, 0.1, 1000);
   updateCameraPosition(cameraPosition);
 
+  //CONTROLS-----------------
+  orbitControls = new OrbitControls(camera, renderer.domElement);
+  orbitControls.addEventListener("end", () => {
+    updateCameraOrbit();
+  });
+  updateCameraOrbit();
+
+  //SCENE--------------------
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x696969);
 
@@ -27,17 +54,21 @@ function createScene(
     scene.add(grid);
   }
 
+  //LIGHT--------------------
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
   scene.add(ambientLight);
 
-  renderer = new THREE.WebGLRenderer({
-    alpha: true,
-    antialias: true,
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
   return scene;
+}
+
+/**
+ * activates animation
+ */
+function animate() {
+  renderScene();
+  orbitControls.update();
+  calculateCameraVectors(); //only for development
+  requestAnimationFrame(animate);
 }
 
 function renderScene() {
@@ -57,26 +88,65 @@ function insertCanvas(container: string | null) {
 }
 
 /**
+ * updates camera / player position
+ * @param position: new camera position
+ */
+function updateCameraPosition(position: THREE.Vector3) {
+  camera.position.set(position.x, position.y, position.z);
+  console.log("updateCameraPosition");
+}
+
+function updateCameraOrbit() {
+  const forward = new THREE.Vector3();
+  camera.getWorldDirection(forward);
+  orbitControls.target.copy(camera.position).add(forward);
+}
+
+/**
+ *
+ * calculates camera vectors
+ * displays them on screen
+ * only for development
+ */
+function calculateCameraVectors() {
+  cameraDirection = new THREE.Vector3();
+  camPositionSpan = document.querySelector("#position");
+  camLookAtSpan = document.querySelector("#lookingAt");
+  // this copies the camera's unit vector direction to cameraDirection
+  camera.getWorldDirection(cameraDirection);
+  // scale the unit vector up to get a more intuitive value
+  cameraDirection.set(
+    cameraDirection.x * 100,
+    cameraDirection.y * 100,
+    cameraDirection.z * 100
+  );
+  // update the onscreen spans with the camera's position and lookAt vectors
+  camPositionSpan.innerHTML = `Position: (${camera.position.x.toFixed(
+    1
+  )}, ${camera.position.y.toFixed(1)}, ${camera.position.z.toFixed(1)})`;
+  camLookAtSpan.innerHTML = `LookAt: (${(
+    camera.position.x + cameraDirection.x
+  ).toFixed(1)}, ${(camera.position.y + cameraDirection.y).toFixed(1)}, ${(
+    camera.position.z + cameraDirection.z
+  ).toFixed(1)})`;
+}
+
+/**
  * updates scene based on screen size
  */
 function updateScene() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+
   renderScene();
 }
 
-/**
- * updates camera / player position
- * @param position: new camera position
- */
-function updateCameraPosition(position: THREE.Vector3) {
-  camera.position.set(position.x, position.y, position.z);
-}
-
+//-------------------------------------------
 export function useSceneFactory() {
   return {
     createScene,
+    animate,
     renderScene,
     insertCanvas,
     updateScene,
