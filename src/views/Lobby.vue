d461e1a4800ababb393a8f7f70610053439426fc
Tim Wahrburg <timfriedrich.wahrburg@student.hs-rm.de>
2021-11-23 15:52:19 +0100

    fix(#28) LobbyComponent added

---
+++ src/views/Lobby.vue
@@ -0,0 +1,27 @@
+<template>
+  <LobbyComponent />
+  <Instructions v-if="showInstructions" :instructions="instructions" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import LobbyComponent from "@/components/LobbyComponent.vue";
+import Instructions from "@/components/Instructions.vue";
+
+export default defineComponent({
+  name: "Lobby",
+  components: { LobbyComponent, Instructions },
+  setup() {
+    // activate to test instructions
+    const showInstructions = ref(false);
+
+    // test data
+    const instructions = [
+      "Willkommen unter den Eichen",
+      "Deine erste Aufgabe erwartet dich",
+      "Finde zur Semester Einführungsveranstaltung",
+    ];
+    return { instructions, showInstructions };
+  },
+});
+</script>
