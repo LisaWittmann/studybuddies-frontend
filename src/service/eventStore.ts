0e7daa1f4a03c267f7080ac4c7c3b4c8f1b6d5c6
Timo Striffler <timo.striffler@student.hs-rm.de>
2021-11-30 13:18:16 +0100

    adjust(#48): documented the functions - changed the websocket target location

--- src/service/eventStore.ts
+++ src/service/eventStore.ts
@@ -1,24 +1,31 @@
+import { reactive, readonly } from '@vue/reactivity';
 import { Client } from '@stomp/stompjs';
 import { useLabyrinthStore } from './LabyrinthStore';
 import { eventMessage } from './eventMessage';
 
 const {labyrinthState, updateLabyrinth} = useLabyrinthStore();
 
-const wsurl = "ws:localhost:9090/messagebroker";
+//const wsurl = "ws://localhost:9090/messagebroker";
 const DEST = "/event/respond";
 
-const stompclient = new Client({ brokerURL: wsurl })
+const stompclient = new Client({/* brokerURL: wsurl */});
+
+/**
+ * Connection Error Feedback for the Stompclient
+ */
 stompclient.onWebSocketError = (/*event*/) => { console.log("websocketerror"); labyrinthState.errormessage = "WS-Fehler" }
 stompclient.onStompError = (/*frame*/) => { console.log("Stomperror"); labyrinthState.errormessage = "STOMP-Fehler" }
 
-
+/**
+ * Stompclient Methode to subscribe the Backend Messages on successful Connection and work with it
+ */
 stompclient.onConnect = (/*frame*/) => {
 
-    console.log("tester");
+    console.log("stomp verbindet");
 
     stompclient.subscribe( DEST, (message) => {
 
-        console.log("tester");
+        console.log("Message ist angekommen");
 
         console.log(JSON.parse(message.body));
 
@@ -30,7 +37,10 @@ stompclient.onConnect = (/*frame*/) => {
     });
 };
 
-
+/**
+ * Methode to handle the Disconnection
+ */
 stompclient.onDisconnect = () => { /* Verbindung abgebaut*/ }
+
 
 stompclient.activate();
