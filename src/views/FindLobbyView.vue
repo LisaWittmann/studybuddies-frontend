6e72202e9f22245aed3e5099e3e017fc1d3b4d07
Sven Spurk <sven.spurk@student.hs-rm.de>
2021-12-01 11:02:11 +0100

    feat(#53): Very basic find/create lobby view

---
+++ src/views/FindLobbyView.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Platzhalter Logo</h1>
+    <form class="login__form">
+        <ul>
+            <li>Spiel finden <input type="text"> <button type="submit">OK</button></li>
+            <li>Spiel erstellen <input type="text" placeholder="FJIE4D (<-- Beispiel-Code)"></li>
+        </ul>
+        
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import router from "@/router";
+
+export default defineComponent({
+    name: "FindLobby",
+    setup() {
+        // Logic here
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        border: 1px solid black;
+        padding: 1em;
+    }
+
+    ul {
+        list-style-type: none;
+    }
+    
+    li {
+        margin: 1em;
+    }
+</style>
