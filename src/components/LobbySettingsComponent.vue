2b5001222f61c2b62bde47b778e401cbd797d1da
Larissa Herter <larissa.herter@student.hs-rm.de>
2021-12-02 12:23:19 +0100

    feat(#56): add dd. and more in template of LobbySC

--- src/components/LobbySettingsComponent.vue
+++ src/components/LobbySettingsComponent.vue
@@ -1,14 +1,21 @@
 <template>
-    <h1>Hallo Welt!</h1>
-  <input type="file" ref="data" id="file-input" @change="dataUpload" /><br />
+    <h1>Lobby Einstellungen</h1>
+    <section class="section">
+        <p>Labyrinth hochladen</p>
+        <input type="file" ref="data" id="file-input" @change="dataUpload" /><br />
+        <p>Labyrinth auswählen</p>
+        <DropdownMenu />
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { useLabyrinthService } from "@/service/LabyrinthService";
+import DropdownMenu from "@/components/DropdownMenu.vue";
 
 export default defineComponent({
   name: "LobbySettingsComponent",
+  components: { DropdownMenu },
   setup() {
     const { uploadJsonFiles } = useLabyrinthService();
     const data = ref({} as HTMLInputElement);
