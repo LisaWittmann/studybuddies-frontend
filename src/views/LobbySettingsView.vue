761b529e59cd3c5b5a5628460c558d91c13513b7
Larissa Herter <larissa.herter@student.hs-rm.de>
2021-12-02 11:13:52 +0100

    feat(#56): add LobbySettingsView

---
+++ src/views/LobbySettingsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <LobbySettingsComponent />
+  <Instructions v-if="showInstructions" :instructions="instructions" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LobbySettingsComponent from "@/components/LobbySettingsComponent.vue";
+
+export default defineComponent({
+  name: "LobbySettingsView",
+  components: { LobbySettingsComponent },
+  setup() {
+    return {  };
+  },
+});
+</script>
