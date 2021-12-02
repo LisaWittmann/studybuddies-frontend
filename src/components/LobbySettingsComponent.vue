e1199ca0cd5fba10b98830ee1aec932ea866fb81
Larissa Herter <larissa.herter@student.hs-rm.de>
2021-12-02 11:14:11 +0100

    feat(#56): add LobbySettingsComponent

---
+++ src/components/LobbySettingsComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Hallo Welt!</h1>
+  <input type="file" ref="data" id="file-input" @change="dataUpload" /><br />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useLabyrinthService } from "@/service/LabyrinthService";
+
+export default defineComponent({
+  name: "LobbySettingsComponent",
+  setup() {
+    const { uploadJsonFiles } = useLabyrinthService();
+    const data = ref({} as HTMLInputElement);
+
+    async function dataUpload() {
+      if (data.value.files != null) {
+        await uploadJsonFiles(data.value.files);
+      }
+    }
+
+    return {
+      data,
+      dataUpload,
+    };
+  },
+});
+</script>
