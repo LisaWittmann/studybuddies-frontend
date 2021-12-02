9d433366488e1375092f40fd0ebcf58593934bd6
Larissa Herter <larissa.herter@student.hs-rm.de>
2021-12-02 17:41:10 +0100

    feat(#56): add some css for dropdown menu

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -58,8 +58,8 @@ export default defineComponent({
 .dropDownMenuWrapper {
   position: relative;
   width: 500px;
-  left: 37vw;
-  height: 30px;
+  margin: 0 auto;
+  height: 80px;
   border-radius: 8px;
   background: white;
   border: 1px solid #eee;
@@ -70,6 +70,31 @@ export default defineComponent({
     box-sizing: border-box;
     text-align: left;
   }
+
+  .dropDownMenuButton {
+    border: none;
+    font-size: inherit;
+    background: none;
+    outline: none;
+    border-radius: 4px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    padding: 0 70px 0 20px;
+    margin: 0;
+    line-height: 1;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    cursor: pointer;
+  }
+
+  .dropDownMenuButton--dark {
+    color: #eee;
+  }
+
   .iconWrapper {
     width: 25px;
     height: 25px;
@@ -92,13 +117,17 @@ export default defineComponent({
       transition: all 0.2s ease;
     }
 
-.bar1--open {
+    .bar1--dark {
+      background: #eee;
+    }
+
+    .bar1--open {
       transform: translate(-50%, -50%) rotate(45deg);
       margin-top: 0;
       background: red;
     }
 
-.bar2 {
+    .bar2 {
       width: 100%;
       max-width: 28px;
       height: 3px;
@@ -111,11 +140,16 @@ export default defineComponent({
       transform: translate(-50%, -50%);
       transition: all 0.2s ease;
     }
-.bar2--open {
+
+    .bar2--dark {
+      background: #eee;
+    }
+
+    .bar2--open {
       opacity: 0;
     }
 
-.bar3 {
+    .bar3 {
       width: 100%;
       max-width: 28px;
       height: 3px;
@@ -126,15 +160,126 @@ export default defineComponent({
       border-radius: 9999px;
       transform: translate(-50%, calc(-50% + 8px) );
       transition: all 0.2s ease;
-}
+    }
 
-.bar3--open {
+    .bar3--dark {
+      background: #eee;
+    }
+
+    .bar3--open {
       top: 50%;
       transform: translate(-50%, -50% ) rotate(-45deg);
       background: red;
     }
 
   }
+
+  .iconWrapper--noTitle {
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: auto;
+    height: auto;
+    transform: none;
+  }
+
+  .dropdownMenu {
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    min-width: 300px;
+    min-height: 10px;
+    border-radius: 8px;
+    border: 1px solid #eee;
+    box-shadow: 10px 10px 0 0 rgba(black,.03);
+    background: white;
+    padding: 10px 30px;
+    animation: menu 0.3s ease forwards;
+
+    .menuArrow {
+      width: 20px;
+      height: 20px;
+      position: absolute;
+      top: -10px;
+      left: 20px;
+      border-left: 1px solid #eee;
+      border-top: 1px solid #eee;
+      background: white;
+      transform: rotate(45deg);
+      border-radius: 4px 0 0 0;
+    }
+
+    .menuArrow--dark {
+      background: #333;
+      border: none;
+    }
+
+    .option {
+      width: 100%;
+      border-bottom: 1px solid #eee;
+      padding: 20px 0;
+      cursor: pointer;
+      position: relative;
+      z-index: 2;
+
+      &:last-child {
+        border-bottom: 0;
+      }
+
+      * {
+        color: inherit;
+        text-decoration: none;
+        background: none;
+        border: 0;
+        padding: 0;
+        outline: none;
+        cursor: pointer;
+      }
+
+    }
+
+    .desc {
+      opacity: 0.5;
+      display: block;
+      width: 100%;
+      font-size: 14px;
+      margin: 3px 0 0 0;
+      cursor: default;
+    }
+
+  }
+
+  .dropdownMenu--dark {
+    background: #333;
+    border: none;
+
+    .option {
+      border-bottom: 1px solid #888;
+    }
+
+    * {
+      color: #eee;
+    }
+
+  }
+
+  @keyframes menu {
+    from { transform: translate3d( 0, 30px ,0 ) }
+    to { transform: translate3d( 0, 20px ,0 ) }
+  }
+
+}
+
+.dropDownMenuWrapper--noTitle {
+  padding: 0;
+  width: 60px;
+  height: 60px;
+}
+
+.dropDownMenuWrapper--dark {
+  background: #333;
+  border: none;
 }
 
 
