5e7d5c00f0bc0f02a5b075bf39588d9ab03db542
Sven Spurk <sven.spurk@student.hs-rm.de>
2021-12-03 14:27:28 +0100

    fix(#53): Added POST call for lobby creation

--- src/views/FindLobbyView.vue
+++ src/views/FindLobbyView.vue
@@ -3,7 +3,7 @@
     <form class="login__form">
         <ul>
             <li>Spiel finden <input type="text"> <button type="submit">OK</button></li>
-            <li>Spiel erstellen <input type="text" placeholder="FJIE4D (<-- Beispiel-Code)"></li>
+            <li>Spiel erstellen <button @click="createGame">Spiel erstellen</button></li>
         </ul>
         
     </form>
@@ -11,12 +11,26 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { User } from "@/service/User";
 import router from "@/router";
 
 export default defineComponent({
     name: "FindLobby",
     setup() {
-        // Logic here
+        const user = new User();
+
+        fetch("/api/lobby/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          }
+        //   body: JSON.stringify(user),
+        })
+          .then((response) => {
+            if (response.ok) router.push("/");
+            // else hasErrors.value = true;
+          })
+          .catch((err) => console.log(err));
     },
 })
 </script>
