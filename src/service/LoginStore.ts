ec4d23169a2378bedee38620c8c73791d6bfc8f4
Sven Spurk <sven.spurk@student.hs-rm.de>
2021-12-04 11:51:01 +0100

    fix(#S84): Code cleanup and error handling fixed

--- src/service/LoginStore.ts
+++ src/service/LoginStore.ts
@@ -1,57 +1,60 @@
 import { reactive, readonly } from "vue";
-import { stringifyQuery } from "vue-router";
 
 const loginstate = reactive({
-    username: '',
-    errormessage: '',
-    isLoggedIn: false
-})
+  username: "",
+  errormessage: "",
+  isLoggedIn: false,
+});
 
 export function doLogout() {
-    loginstate.username = ''
-    loginstate.errormessage = ''
-    loginstate.isLoggedIn = false
+  loginstate.username = "";
+  loginstate.errormessage = "";
+  loginstate.isLoggedIn = false;
 }
 
-export async function doLogin(user: string, password: string): Promise<boolean> {
-    console.log('doLogin() called')
-    try {
-        const userObj = {
-            username: user,
-            password: password
-        }
+export async function doLogin(user: string, pass: string): Promise<boolean> {
+  try {
+    const userObj = {
+      username: user,
+      password: pass,
+    };
 
-        console.log(`UserObj: ${userObj}`)
-    
-        const response = await fetch('/api/login', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json;charset=utf-8"
-            },
-            body: JSON.stringify(userObj)
-        })
-    
-        // API Call verarbeiten
-        const responseData = await response.json
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(userObj),
+    });
 
-        // Testing
-        loginstate.username = user
-        loginstate.errormessage = ''
-        loginstate.isLoggedIn = true
-
-        return true
-
-    } catch(reason) {
-        loginstate.errormessage = String(reason)
-
-        return false
+    if (!response.ok) {
+      console.error("ERROR");
+      loginstate.errormessage = "Fehler beim einloggen!";
+      return false;
     }
+
+    const responseData = response.json();
+    console.log(responseData);
+
+    loginstate.username = user;
+    loginstate.errormessage = "";
+    loginstate.isLoggedIn = true;
+
+    console.log(loginstate);
+
+    return true;
+  } catch (reason) {
+    console.warn(`Catch activated`);
+    loginstate.errormessage = String(reason);
+
+    return false;
+  }
 }
 
 export function useLoginStore() {
-    return {
-        loginstate: readonly(loginstate),
-        doLogin,
-        doLogout
-    }
+  return {
+    loginstate: readonly(loginstate),
+    doLogin,
+    doLogout,
+  };
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,61 +6,9 @@
         <h2>Bitte gib deinen Benutzernamen und dein Passwort ein</h2>
       </div>
       <div class="login__form-wrapper">
-        <input type="text" name="username" v-model="username">
-        <input type="password" name="password" v-model="password">
+        <input type="text" name="username" v-model="username" />
+        <input type="password" name="password" v-model="password" />
         <button @click="handleLogin(username, password)">Login</button>
-        <!-- <form
-          v-if="inLoginState"
-          @submit.prevent="login(user)"
-          class="login__form"
-        >
-          <span v-if="usernameEmpty" class="error">{{
-            emptyUsernameMessage
-          }}</span>
-          <input
-            type="username"
-            placeholder="Benutzername"
-            v-model="user.username"
-          />
-
-          <span v-if="passwordEmpty" class="error">{{
-            emptyPasswordMessage
-          }}</span>
-          <input
-            type="password"
-            placeholder="Passwort"
-            v-model="user.password"
-          />
-
-          <button type="submit">Anmelden</button>
-          <p>
-            Noch kein Benutzerkonto?
-            <a @click="switchToRegister">Jetzt registrieren</a>
-          </p>
-        </form>
-        <form v-else @submit.prevent="register(user)" class="login__form">
-          <span v-if="usernameEmpty" class="error">{{
-            emptyUsernameMessage
-          }}</span>
-          <input
-            type="username"
-            placeholder="Benutzername"
-            v-model="user.username"
-          />
-          <span v-if="passwordEmpty" class="error">{{
-            emptyPasswordMessage
-          }}</span>
-          <input
-            type="password"
-            placeholder="Passwort"
-            v-model="user.password"
-          />
-          <button type="submit">Registrieren</button>
-          <p>
-            Du bist bereits registriert?
-            <a @click="switchToLogin">Jetzt anmelden</a>
-          </p>
-        </form> -->
       </div>
     </div>
   </div>
@@ -68,129 +16,35 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-// import { User } from "@/service/User";
 import { useLoginStore } from "@/service/LoginStore";
 import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "Login",
   setup() {
-    // handles login/register state
-    // const inLoginState = ref(false);
-    // const user = new User();
-    const router = useRouter()
-    const username = ''
-    const password = ''
+    const router = useRouter();
+    const username = "";
+    const password = "";
 
-    const hasErrors = ref(false);
-    // const errorMessage = ref("testing error message");
-    const {
-      loginstate,
-      doLogin,
-      doLogout
-    } = useLoginStore()
+    const { loginstate, doLogin, doLogout } = useLoginStore();
 
-    onMounted(async() => {
-      await doLogout()
-    })
+    onMounted(async () => {
+      await doLogout();
+    });
 
     async function handleLogin(username: string, password: string) {
-      console.log(`Username: ${username}\nPass: ${password}`)
-      await doLogin(username, password)
-      await router.push('/lobby')
+      console.log(`Username: ${username}\nPass: ${password}`);
+      const loggedIn = await doLogin(username, password);
+      if (loggedIn) {
+        router.push("/lobby");
+      }
     }
 
-    // const usernameEmpty = ref(false);
-    // const passwordEmpty = ref(false);
-
-    // const emptyUsernameMessage = "Benutzername darf nicht leer sein";
-    // const emptyPasswordMessage = "Passwort darf nicht leer sein";
-
-    // function switchToRegister() {
-    //   // loginstate.isLoggedIn.value = false;
-    //   hasErrors.value = false;
-    // }
-
-    // function switchToLogin() {
-    //   // inLoginState.value = true;
-    //   hasErrors.value = false;
-    // }
-
-    // Refactor entire logic into computed
-    // function validateProcess() {
-    //   if (!loginstate.username) {
-    //     usernameEmpty.value = true;
-    //     hasErrors.value = true;
-    //   } else {
-    //     usernameEmpty.value = false;
-    //   }
-
-    //   if (!loginstate.) {
-    //     passwordEmpty.value = true;
-    //     hasErrors.value = true;
-    //   } else {
-    //     passwordEmpty.value = false;
-    //   }
-
-    //   if (user.username && user.password) {
-    //     usernameEmpty.value = false;
-    //     passwordEmpty.value = false;
-    //     hasErrors.value = false;
-    //     return true;
-    //   }
-
-    //   return false;
-    // }
-
-    // function login() {
-    //     fetch("/api/login", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json;charset=utf-8",
-    //       },
-    //       body: JSON.stringify(loginstate.username),
-    //     })
-    //       .then((response) => {
-    //         if (response.ok) router.push("/lobby");
-    //         else hasErrors.value = true;
-    //       })
-    //       .catch((err) => console.log(err));
-    //   }
-
-    // function register() {
-    //     fetch("/api/register", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json;charset=utf-8",
-    //       },
-    //       body: JSON.stringify(loginstate.username),
-    //     })
-    //       .then((response) => {
-    //         if (response.ok) switchToLogin();
-    //         else hasErrors.value = true;
-    //       })
-    //       .catch((err) => console.log(err));
-    //   }
-
-
-
     return {
-      // user,
-      // login,
-      // register,
-      // // inLoginState,
-      // switchToRegister,
-      // switchToLogin,
-      // hasErrors,
-      // // errorMessage,
-      // emptyUsernameMessage,
-      // emptyPasswordMessage,
-      // usernameEmpty,
-      // passwordEmpty,
       username,
       password,
       handleLogin,
-      loginstate
+      loginstate,
     };
   },
 });
