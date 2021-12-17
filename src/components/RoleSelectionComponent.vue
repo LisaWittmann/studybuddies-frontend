339b6d977cf427024aa329733c5b6a7d75ea1e63
laherter <larissa.herter@student.hs-rm.de>
2021-12-17 15:27:43 +0100

    feat(#101): add radio button for picking role

---
+++ src/components/RoleSelectionComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <label :for="option">
+    <input
+      type="radio"
+      :value="option"
+      :id="option"
+      @change="$parent.$emit('input', $event.target.value)"
+      name="radio-input"
+    />
+    {{ option }}
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        option: {type: String, required: true}
+    }
+});
+</script>
+
+
+<style scoped>
+
+</style>
