4aa47740a2175c67b94d270f524473674364b3bc
laherter <larissa.herter@student.hs-rm.de>
2021-12-17 15:28:35 +0100

    feat(#101): add RadioButtonGroup for diff role options

---
+++ src/components/RadioButtonGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="input-group">
+    <RoleSelectionComponent v-for="option in options" :option="option" :key="option" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import RoleSelectionComponent from "./RoleSelectionComponent.vue";
+
+export default defineComponent({
+    name: "RadioButtonGroup",
+    components: { RoleSelectionComponent },
+    props: { 
+        options: {
+            type: Array, 
+            required: true}
+     }
+});
+</script>
+
+<style scoped>
+.input-group {
+  width: 35%;
+  margin: 20px auto;
+  padding: 15px 20px;
+  border: 1px solid black;
+  border-radius: 8px;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+label {
+  cursor: pointer;
+}
+</style>
